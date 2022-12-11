e1d368e8884c7364d884741c85ab2d87b235c5f8
Valande <valande@gmail.com>
2022-12-11 08:14:17 +0100

    Install MariaDB and PHP deps

--- provision/wp/pkginstall.sh
+++ provision/wp/pkginstall.sh
@@ -5,3 +5,11 @@
 #: Software provisioning script for Wordpress VM
 #:
 
+# Install and secure MariaDB database
+apt install -y mariadb-server
+
+# Install PHP dependencies
+apt install -y php-fpm php-mysql expect php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+
+
+# Configure nginx
