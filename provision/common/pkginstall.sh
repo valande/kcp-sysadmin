90b00eed68d346dfd765531f49d32c8b1b24cea0
Valande <valande@gmail.com>
2022-12-20 18:18:51 +0100

    Add elastic repo from common provisioning

--- provision/common/pkginstall.sh
+++ provision/common/pkginstall.sh
@@ -10,3 +10,11 @@ apt update -y
 
 # Install nginx
 apt install -y nginx
+
+# Add and enable elastic.co packages repository
+if [ ! -f /etc/apt/sources.list.d/elastic-8.x.list ]; then
+    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+    apt-get install apt-transport-https
+    echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-8.x.list
+    apt update -y
+fi
